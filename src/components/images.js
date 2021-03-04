c52f47c1e814d5e88e644bf155238d24a81003e1
Rudinei lopes <rudinei.lopes@hotmail.com>
2021-03-03 21:50:58 -0300

    at deploy

--- src/components/images.js
+++ src/components/images.js
@@ -3,7 +3,7 @@
 
 export default [
     "https://4.bp.blogspot.com/-fXQUf52mB0I/XO98d2WF4ZI/AAAAAAAAGls/P3BtEoQqEoszt_NmeXJxyi2Tx9rB_qJeACKgBGAs/w0/galaxy-slayer-zed-lol-4K-97.jpg",
-    "./wallpapers/wall3.jpg",
+    "https://ibb.co/y8Bjbgj",
     "./wallpapers/wall4.jpg",
     "./wallpapers/wall5.jpg",
     "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_2.jpg",
