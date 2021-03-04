4117aefecdb95342909336170dd7703bf92e9c6d
Rudinei lopes <rudinei.lopes@hotmail.com>
2021-03-03 22:07:41 -0300

    atualização

--- src/components/images.js
+++ src/components/images.js
@@ -4,8 +4,8 @@
 export default [
     "https://4.bp.blogspot.com/-fXQUf52mB0I/XO98d2WF4ZI/AAAAAAAAGls/P3BtEoQqEoszt_NmeXJxyi2Tx9rB_qJeACKgBGAs/w0/galaxy-slayer-zed-lol-4K-97.jpg",
     "https://i.ibb.co/D7g2Sw2/wall3.jpg",
-    "https://i.ibb.co/D7g2Sw2/wall3.jpg",
-    "https://i.ibb.co/D7g2Sw2/wall3.jpg",
+    "https://i.ibb.co/V2x9hDT/wall8.jpg",
+    "https://i.ibb.co/NLGfHJ6/wall12.jpg",
     "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_2.jpg",
     "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_8.jpg",
     "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Leona_0.jpg",
