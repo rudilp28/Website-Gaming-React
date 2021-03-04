d1b7a1fde0063f9ae5c2699fc4bd8f13fd03d4cb
Rudinei lopes <rudinei.lopes@hotmail.com>
2021-03-03 21:38:55 -0300

    Updates

---
+++ precache-manifest.4df989e3cfa55b753b1d14e64f239ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b162a17a8fd6f586df35d2aac7da160",
+    "url": "/Website-Gaming-React/index.html"
+  },
+  {
+    "revision": "3200ca2fe351cbae1db3",
+    "url": "/Website-Gaming-React/static/css/main.8d5939c9.chunk.css"
+  },
+  {
+    "revision": "7f40e6bd6f4b94a8b499",
+    "url": "/Website-Gaming-React/static/js/2.67bff073.chunk.js"
+  },
+  {
+    "revision": "a2aa3c5340df792b109bc98711a48e34",
+    "url": "/Website-Gaming-React/static/js/2.67bff073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3200ca2fe351cbae1db3",
+    "url": "/Website-Gaming-React/static/js/main.e3252325.chunk.js"
+  },
+  {
+    "revision": "ad968f5312b594c6618c",
+    "url": "/Website-Gaming-React/static/js/runtime-main.73e6b823.js"
+  }
+]);
